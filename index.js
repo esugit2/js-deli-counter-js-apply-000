988b58706e36484843bd97729a7d3c4929f2e511
Edward Sugitpibul <etsugitpibul@gmail.com>
2019-08-18 15:44:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`; 
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`;
+  } else {
+    var serving = `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
+    return serving;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return `The line is currently empty.`;
+  } else {
+    var array = []
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      array.push(` ${i+1}. ${katzDeliLine[i]}`);
+    }
+    return `The line is currently:` + array;
+  }
+}
